06b342c3c3277a8336b2e86599e9b254007ccab8
StanleyIG <oootehts@gmail.com>
2024-04-27 17:06:13 +0300

    stage_4 start

--- app/main.py
+++ app/main.py
@@ -12,21 +12,26 @@ app = FastAPI(
     root_path="/api",
 )
 
-app.include_router(router_bookings)
+#app.include_router(router_bookings)
 
-# class SBooking(BaseModel):
-#     room_id: int
-#     date_from: datetime
-#     date_to: datetime
+class SBooking(BaseModel):
+    room_id: int
+    date_from: datetime
+    date_to: datetime
 
+class SBookingPublic(BaseModel):
+    date_from: datetime
+    date_to: datetime
+    qwerty: int
 # @app.get('/hotels/{id}')
 # def get_hotels(search_args: HotelSearchArgs = Depends()):
 #     return search_args
 
 
-# @app.post('/bookings')
-# def add_booking(booking: Annotated[SBooking, Depends()]) -> list[SBooking]:
-#     return [booking]                                                        
+@app.post('/bookings')
+def add_booking(booking: Annotated[SBooking, Depends()]) -> list[SBookingPublic]:
+    d = {'id': 1, "date_from": "2024-04-27T00:00:00", "date_to": "2024-04-27T00:00:00", 'qwe': 123}
+    return [d]                                                        
 
 # list[SBooking] для валидации отдаваемых данных
 # как более лаконичный способ вместо response_model = list[SBooking]
