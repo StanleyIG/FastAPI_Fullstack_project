d9bf5a4404839ad82445b1d283a94b2d19a9dd9e
StanleyIG <oootehts@gmail.com>
2024-04-30 12:46:19 +0300

    add all routes and templates, static, redis cache and CORS midleware

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI, Depends
-from fastapi.requests import Request
-from pydantic import BaseModel
-from typing import Annotated
-from settings import HotelSearchArgs
-from datetime import datetime
-from app.bookings.router import router as router_bookings
-from app.users.router import router_auth, router_users
+from contextlib import asynccontextmanager
+import time
 
+import sentry_sdk
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
+from app.bookings.router import router as router_bookings
+from app.config import settings
+from app.database import engine
+from app.hotels.router import router as router_hotels
+from app.hotels.rooms.router import router as router_rooms
+from app.images.router import router as router_images
+from app.importer.router import router as router_import
+from app.logger import logger
+from app.pages.router import router as router_pages
+from app.users.router import router_auth, router_users
 
 app = FastAPI(
     title="Бронирование Отелей",
@@ -14,30 +25,72 @@ app = FastAPI(
     root_path="/api",
 )
 
+
+
+# подключение роутеров
+app.include_router(router_auth)
+app.include_router(router_users)
+app.include_router(router_hotels)
+app.include_router(router_rooms)
 app.include_router(router_bookings)
 
-
-class SBooking(BaseModel):
-    room_id: int
-    date_from: datetime
-    date_to: datetime
+app.include_router(router_images)
+app.include_router(router_import)
 
 
-class SBookingPublic(BaseModel):
-    date_from: datetime
-    date_to: datetime
-    qwerty: int
-# @app.get('/hotels/{id}')
-# def get_hotels(search_args: HotelSearchArgs = Depends()):
-#     return search_args
+# Подключение CORS, чтобы запросы к API могли приходить из браузера 
+origins = [
+    # порт 3000 для React.js например
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", 
+                   "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    "подключение Redis"
+
+    redis = aioredis.from_url(
+        f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+        encoding="utf8",
+        decode_responses=True,
+    )
+    FastAPICache.init(RedisBackend(redis), prefix="cache")
+    yield
+
+    
+app.include_router(router_pages)
+
+# Подключение статики
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 
-# @app.post('/bookings')
-# def add_booking(booking: Annotated[SBooking, Depends()]) -> list[SBookingPublic]:
-#     d = {'id': 1, "date_from": "2024-04-27T00:00:00",
-#          "date_to": "2024-04-27T00:00:00", 'qwe': 123}
-#     return [d]
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    logger.info("Request handling time", extra={
+        "process_time": round(process_time, 4)
+    })
+    return response
 
-# list[SBooking] для валидации отдаваемых данных
-# как более лаконичный способ вместо response_model = list[SBooking]
-# в праметрах контроллера
+
+if __name__ == "__main__":
+    import uvicorn
+
+    import os.path
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+    uvicorn.run(
+        app="app.main:app",
+        reload=True,
+    )
---
+++ app/tasks/celery.py
@@ -0,0 +1,30 @@
+from celery import Celery
+from celery.schedules import crontab
+
+from app.config import settings
+
+celery = Celery(
+    "tasks",
+    broker=f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+    include=[
+        "app.tasks.tasks",
+        "app.tasks.scheduled",
+    ]
+)
+
+# Расписание запуска задач для celery beat
+celery.conf.beat_schedule = {
+    "email.booking_reminder_1day": {
+        "task": "email.booking_reminder_1day",
+        "schedule": crontab(minute="0", hour="9"),  # каждое утро в 9:00
+    },
+    "email.booking_reminder_3days": {
+        "task": "email.booking_reminder_3days",
+        "schedule": crontab(minute="0", hour="13"),  # каждый день в 13:00
+    },
+    "luboe-nazvanie": {
+        "task": "periodic_task",
+        "schedule": 5,  # секунды
+        # "schedule": crontab(minute="30", hour="15"),
+    }
+}
